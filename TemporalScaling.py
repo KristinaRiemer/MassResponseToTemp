c628247b09525f223aa7cbb5bb49992122a8aeda
Kristina Riemer <reamy316@aim.com>
2014-11-14 13:22:25 -0700

    Attempts at extracting data by year from temp .nc file

--- TemporalScaling.py
+++ TemporalScaling.py
@@ -1,14 +1,46 @@
 from __future__ import division
 
-# read in individual and species data
+# Read in individual and species data
 import pandas as pd
 individual_data = pd.read_csv("FinalSpeciesDataset.csv")
 species_data = pd.read_csv("FinalSpeciesList.csv")
 
-# read in temperature data
+# Read in temperature data
+# http://www.esrl.noaa.gov/psd/data/gridded/data.UDel_AirT_Precip.html
 import numpy as np
 import matplotlib.pyplot as plt
 import netCDF4
+temperature_data = netCDF4.Dataset("air.mon.mean.v301.nc", mode="r")
 
-temperature_data = netCDF4.Dataset("air.mon.mean.v301.nc")
+# Read in temperature data variables
+lat = nc.variables["lat"][:]
+lon = nc. variables["lon"][:]
+times = nc.variables["time"]
+
+
+
+#jd = netCDF4.num2date(times[:],times.units)
+
+def near(array,value):
+    idx=(np.abs(array-value)).argmin()
+    return idx
+
+ix = near(lon, loni)
+iy = near(lat, lati)
+
+vname = "mer_mer_mer"
+var = nc.variables[vname]
+h = var[:,iy,ix]
+
+plt.figure(figsize=(16,4))
+plt.plot_date(jd,h,fmt='-')
+plt.grid()
+plt.ylabel(var.units)
+plt.title("baa_baa_baa")
+    
+
+# Read in temperature data try 2
+from osgeo import gdal
+temperature_data2 = gdal.Open("air.mon.mean.v301.nc")
+print temperature_data2.GetMetadata()
 
