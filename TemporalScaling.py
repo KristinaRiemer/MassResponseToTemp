de3bfb4cda599dcfa8cc106dcc36ff0daebf07c1
Kristina Riemer <reamy316@aim.com>
2014-11-28 08:24:50 -0700

    GDAL netCDF works in Anaconda, add GDAL commands to file

--- TemporalScaling.py
+++ TemporalScaling.py
@@ -7,45 +7,28 @@ species_data = pd.read_csv("FinalSpeciesList.csv")
 
 # Read in temperature data
 # http://www.esrl.noaa.gov/psd/data/gridded/data.UDel_AirT_Precip.html
-import numpy as np
-import matplotlib.pyplot as plt
-import netCDF4
-temperature_data = netCDF4.Dataset("air.mon.mean.v301.nc", mode="r")
-
-# Read in temperature data variables
-lat = nc.variables["lat"][:]
-lon = nc. variables["lon"][:]
-times = nc.variables["time"]
-
-
-
-#jd = netCDF4.num2date(times[:],times.units)
-
-def near(array,value):
-    idx=(np.abs(array-value)).argmin()
-    return idx
-
-ix = near(lon, loni)
-iy = near(lat, lati)
-
-vname = "mer_mer_mer"
-var = nc.variables[vname]
-h = var[:,iy,ix]
-
-plt.figure(figsize=(16,4))
-plt.plot_date(jd,h,fmt='-')
-plt.grid()
-plt.ylabel(var.units)
-plt.title("baa_baa_baa")
-    
-
-# Read in temperature data try 2
 from osgeo import gdal
 from osgeo.gdalconst import *
 gdal.AllRegister()
 driver = gdal.GetDriverByName("netCDF")
-# Error 4: doesn't recognize .nc file formats
-# Changed to full path name, same result
-temperature_data2 = gdal.Open("air.mon.mean.v301.nc")
-print temperature_data2.GetMetadata()
+temperature_data = gdal.Open("air.mon.mean.v301.nc")
 
+# Useful GDAL commands
+#print temperature_data2.GetMetadata()
+cols = temperature_data.RasterXSize
+rows = temperature_data.RasterYSize
+bands = temperature_data.RasterCount
+
+# Get info about file
+temperature_data_geotransform = temperature_data.GetGeoTransform()
+originX = temperature_data_geotransform[0]
+originY = temperature_data_geotransform[3]
+pixelWidth = temperature_data_geotransform[1]
+pixelHeight = temperature_data_geotransform[5]
+
+# Specify which band, i.e., year, read in that band, and get specific value
+# Not getting expected values, should be temperatures
+band = temperature_data.GetRasterBand(1000)
+bandtype = gdal.GetDataTypeName(band.DataType)
+data = band.ReadAsArray(0, 0, cols, rows)
+value = data[40, 237]
