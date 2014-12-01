8a86e444ef1950eb29f08b261955129dc4bdca47
Kristina Riemer <reamy316@aim.com>
2014-12-01 11:36:27 -0700

    More failed attempts to get correct temp data at single location and time

--- TemporalScaling.py
+++ TemporalScaling.py
@@ -21,14 +21,36 @@ bands = temperature_data.RasterCount
 
 # Get info about file
 temperature_data_geotransform = temperature_data.GetGeoTransform()
-originX = temperature_data_geotransform[0]
-originY = temperature_data_geotransform[3]
-pixelWidth = temperature_data_geotransform[1]
-pixelHeight = temperature_data_geotransform[5]
+originX = temperature_data_geotransform[0]    #top left x
+originY = temperature_data_geotransform[3]    #top left y
+pixelWidth = temperature_data_geotransform[1]    #west-east resolution
+pixelHeight = temperature_data_geotransform[5]    #north-south resolution
 
 # Specify which band, i.e., year, read in that band, and get specific value
 # Not getting expected values, should be temperatures
-band = temperature_data.GetRasterBand(1000)
+# Choose these x and y values?
+#x = 236.07
+#y = 40.71
+#xOffset = int((x - originX) / pixelWidth)
+#yOffset = int((y - originY) / pixelHeight)
+
+band = temperature_data.GetRasterBand(283)
 bandtype = gdal.GetDataTypeName(band.DataType)
 data = band.ReadAsArray(0, 0, cols, rows)
-value = data[40, 237]
+value = data[40.71, 236.07]
+
+import matplotlib.pyplot as plt
+plt.plot(data)
+
+
+def get_value_at_point(rasterfile, pos):
+    gdata = gdal.Open(rasterfile)
+    gt = gdata.GetGeoTransform()
+    data = gdata.ReadAsArray()
+    gdata = None
+    x = int((pos[0] - gt[0])/gt[1])
+    y = int((pos[1] - gt[3])/gt[5])
+    return data[y, x] 
+
+p = (236.07, 40.71)
+get_value_at_point(temperature_data, p)
