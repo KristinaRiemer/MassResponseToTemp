4bad71d43bd4e189551fcfbf4a1bd9d9b5eb8e81
Kristina Riemer <kristina.riemer@weecology.org>
2015-03-17 19:34:02 -0600

    Start testing for linear regression bug
    Linear regression lines always positive and r2 values high, begin
    tests with test datasets.
    
    Also include code timing and larger data subset.

--- TemporalScaling.py
+++ TemporalScaling.py
@@ -6,11 +6,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import statsmodels.api as sm
+import time
+
+# Timing code run
+initial_time = time.time()
 
 # Datasets
 individual_data = pd.read_csv("CompleteDatasetUS.csv")
-individual_data_subset = individual_data.iloc[0:10]
-individual_data_subset = individual_data_subset[individual_data_subset["genus_species"] != "Microtus ochrogaster"]
+# Larger subset is 0:50 without 39
+individual_data_subset = individual_data.iloc[0:10].drop([4])
+individual_data_subset.index = range(len(individual_data_subset))
 
 gdal.AllRegister()
 driver = gdal.GetDriverByName("netCDF")
@@ -205,6 +210,15 @@ def get_r2_list(dataset, first_variable, second_vari_list):
             r2_list.append(r2)
     return r2_list
 
+# R2 test
+r2_test_data = [[1, 1, 4], [2, 2, 3], [3, 3, 2], [4, 4, 1]]
+r2_test_data = pd.DataFrame(r2_test_data, columns = ["first_variable", "second_vari_1", "second_vari_2"])
+second_vari_list = ["second_vari_1", "second_vari_2"]
+
+plot_linreg(r2_test_data, r2_test_data["first_variable"], second_vari_list, "test")
+
+r2_test = get_r2_list(r2_test_data, r2_test_data["first_variable"], second_vari_list)
+
 def get_slope_list(dataset, first_variable, second_vari_list):
     """Get R^2 values for linear regression of one variable with a second 
     variable across many scales or lags
@@ -318,3 +332,7 @@ species_list, final_slope = get_multiple_stat_lists(get_slope_list, max_past_yea
 
 # Create PDF containing fig for each species of past year and r2 value/slope
 create_stats_fig("all_stats_fig.pdf", species_list, final_r2, final_slope, "past_year")
+
+# Timing code run
+final_time = time.time()
+total_time = (final_time - initial_time) / 60
