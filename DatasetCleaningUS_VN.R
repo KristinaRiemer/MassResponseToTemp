b37af580cb8e21127af67652beeaeeeeacf0f490
Kristina Riemer <kristina.riemer@weecology.org>
2015-11-19 16:59:07 -0500

    Working out regex to extract mass values in VN

--- DatasetCleaningUS_VN.R
+++ DatasetCleaningUS_VN.R
@@ -29,21 +29,36 @@ extract_component = function(dataset_column, regex){
 
 #-----FUNCTIONS ON ENTIRE DATASET------
 
-# How many instances of "weight" are there? Does this match the number of mass values? 
-# Extract mass values for each individual in entire dataset
-individual_data_subset$mass = extract_component(individual_data_subset$dynamicproperties, "weight: ([0-9.]*) g")
-individual_data_subset$mass2 = extract_component(individual_data_subset$dynamicproperties, "weightInGrams: ([0-9.]*)")
-individual_data_subset$mass3 = extract_component(individual_data_subset$dynamicproperties, "weight=([0-9.]*) g")
-individual_data_subset$mass4 = extract_component(individual_data_subset$dynamicproperties, "weight=([0-9.]*)")
+# weight_only = individual_data_subset[grep("weight", individual_data_subset$dynamicproperties), ]
 
 
-# Possible weight combos: "weight: xx g", "weightInGrams: xx", "weight=xx g",  
-
-"Specimen Weight: ([0-9.]*)g"
 
 
-length(grep("weight", individual_data_subset$dynamicproperties))
-sum(!is.na(individual_data_subset$mass))
-sum(!is.na(individual_data_subset$mass2))
-sum(!is.na(individual_data_subset$mass4))
+# # How many instances of "weight" are there? Does this match the number of mass values? 
+# # Extract mass values for each individual in entire dataset
+# individual_data_subset$mass = extract_component(individual_data_subset$dynamicproperties, "weight: ([0-9.]*) g")
+# individual_data_subset$mass2 = extract_component(individual_data_subset$dynamicproperties, "weightInGrams: ([0-9.]*)")
+# individual_data_subset$mass3 = extract_component(individual_data_subset$dynamicproperties, "weight=([0-9.]*) g")
+# individual_data_subset$mass4 = extract_component(individual_data_subset$dynamicproperties, "weight=([0-9.]*)")
+# individual_data_subset$mass5 = extract_component(individual_data_subset$dynamicproperties, "weight=([0-9.]*)g")
+# individual_data_subset$mass6 = extract_component(individual_data_subset$dynamicproperties, "weight: ([0-9.]*)g")
+# 
+# # Possible weight combos: "weight: xx g", "weightInGrams: xx", "weight=xx g",  
+# 
+# "Specimen Weight: ([0-9.]*)g"
+
+
+
+
+working_regex = "weight(?:InGrams)?[:=] ?([0-9.]*)"
+individual_data_subset$mass = extract_component(individual_data_subset$dynamicproperties, working_regex)
+
+
+length(grep("weight", individual_data_subset$dynamicproperties)) - sum(!is.na(individual_data_subset$mass))
+sum(grepl("weight", individual_data_subset$dynamicproperties) & is.na(individual_data_subset$mass))
+
+grepl("weight", individual_data_subset$dynamicproperties) & is.na(individual_data_subset$mass)
+
+# Missing two, problem is there's a "weight=[xx]" that's being picked up first, and then a "weight: xx"
+
 
