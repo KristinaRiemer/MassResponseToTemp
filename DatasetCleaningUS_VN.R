6c7693ed08f55b9b0ff2f7468b49553220bde314
Kristina Riemer <kristina.riemer@weecology.org>
2015-11-22 17:08:38 -0500

    Complete Vertnet mass extraction

--- DatasetCleaningUS_VN.R
+++ DatasetCleaningUS_VN.R
@@ -2,7 +2,6 @@
 
 # Read in Vertnet dataset (size metric only)
 individual_data = read.csv("Vertnet_size.csv")
-individual_data_subset = individual_data[100:300, 151:152]
 
 
 #-----FUNCTIONS-----
@@ -29,36 +28,8 @@ extract_component = function(dataset_column, regex){
 
 #-----FUNCTIONS ON ENTIRE DATASET------
 
-# weight_only = individual_data_subset[grep("weight", individual_data_subset$dynamicproperties), ]
-
-
-
-
-# # How many instances of "weight" are there? Does this match the number of mass values? 
-# # Extract mass values for each individual in entire dataset
-# individual_data_subset$mass = extract_component(individual_data_subset$dynamicproperties, "weight: ([0-9.]*) g")
-# individual_data_subset$mass2 = extract_component(individual_data_subset$dynamicproperties, "weightInGrams: ([0-9.]*)")
-# individual_data_subset$mass3 = extract_component(individual_data_subset$dynamicproperties, "weight=([0-9.]*) g")
-# individual_data_subset$mass4 = extract_component(individual_data_subset$dynamicproperties, "weight=([0-9.]*)")
-# individual_data_subset$mass5 = extract_component(individual_data_subset$dynamicproperties, "weight=([0-9.]*)g")
-# individual_data_subset$mass6 = extract_component(individual_data_subset$dynamicproperties, "weight: ([0-9.]*)g")
-# 
-# # Possible weight combos: "weight: xx g", "weightInGrams: xx", "weight=xx g",  
-# 
-# "Specimen Weight: ([0-9.]*)g"
-
-
-
-
-working_regex = "weight(?:InGrams)?[:=] ?([0-9.]*)"
-individual_data_subset$mass = extract_component(individual_data_subset$dynamicproperties, working_regex)
-
-
-length(grep("weight", individual_data_subset$dynamicproperties)) - sum(!is.na(individual_data_subset$mass))
-sum(grepl("weight", individual_data_subset$dynamicproperties) & is.na(individual_data_subset$mass))
-
-grepl("weight", individual_data_subset$dynamicproperties) & is.na(individual_data_subset$mass)
-
-# Missing two, problem is there's a "weight=[xx]" that's being picked up first, and then a "weight: xx"
-
-
+# Extract mass values for each individual that has mass information
+mass_regex = "weight(?:InGrams)?(?: in g)?\"?[:=] ?\"?\\[?([0-9.]*)"
+individual_data$mass = extract_component(individual_data$dynamicproperties, mass_regex)
+length(grep("weight", individual_data$dynamicproperties)) - sum(!is.na(individual_data$mass))
+sum(grepl("weight", individual_data$dynamicproperties) & is.na(individual_data$mass))
