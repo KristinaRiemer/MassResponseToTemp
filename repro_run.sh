7ed9512effa2a4d6b1023301dc3c3c0810004562
Kristina Riemer <kristina.riemer@weecology.org>
2017-04-03 06:43:28 -0700

    Finish pipeline shell script

--- repro_run.sh
+++ repro_run.sh
@@ -1,7 +1,21 @@
+#run entire project pipeline
+
+#download and clean data
 mkdir data
 R CMD BATCH Cleaning_VN.R
+
+#species relationships for past temperatures
+#time: ~42 hours
+mkdir results_TL
+python Analysis_VN_TL.py
+
+#species relationships for current temperature
 mkdir results
 python Analysis_VN_CY.py
 
-#depends on having TL data
-#R CMD BATCH Visualization_VN_CY.R
+#current temperature relationship viz
+mkdir figures
+R CMD BATCH Visualization_VN_CY.R
+
+#past temperatures relationship viz
+R CMD BATCH Visualization_VN_CY_supp.R
